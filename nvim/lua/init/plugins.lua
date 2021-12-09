03bdc22948d3768e83c20175c917b0505db2a0b8
Ryan-Kalinowski <ryankalinowski1@gmail.com>
2021-12-09 16:07:47 -0500

    gitsigns

--- nvim/lua/init/plugins.lua
+++ nvim/lua/init/plugins.lua
@@ -51,7 +51,46 @@ return require('packer').startup(function(use)
   use {
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
+    config = function()
+      require('gitsigns').setup({
+        signs = {
+          add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+          change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+          delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+          topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+          changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        },
+        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+        current_line_blame_opts = {
+          delay = 100,
+        },
+        max_file_length = 40000,
+        preview_config = {
+          border   = 'none',
+          focusable = true,
+        },
+        keymaps = {
+          noremap = true,
+          ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
+          ['n <leader>gj'] = '<cmd>Gitsigns next_hunk<CR>',
+          ['n <leader>gk'] = '<cmd>Gitsigns prev_hunk<CR>',
+          ['n <leader>ghs'] = '<cmd>Gitsigns stage_hunk<CR>',
+          ['n <leader>ghp'] = '<cmd>Gitsigns preview_hunk<CR>',
+
+          -- Text objects
+          ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
+          ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>',
+        },
+      })
+      vim.cmd([[
+        highlight GitSignsAdd ctermfg=green ctermbg=black
+        highlight GitSignsChange ctermfg=Magenta ctermbg=black
+        highlight GitSignsDelete ctermfg=red ctermbg=black
+      ]])
+    end,
   }
 
   -- Automatically set up configuration after cloning packer.nvim
